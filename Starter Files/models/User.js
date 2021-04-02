9097ae6c6555fb1f0e40683d5ac6f75142d77070
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-02 11:34:06 -0500

    feat(User.js): Created a Schema for the register template

--- Starter Files/models/User.js
+++ Starter Files/models/User.js
@@ -4,10 +4,25 @@ mongoose.Promise = global.Promise;
 const md5 = require('md5');
 const validator = require('validator');
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('password-local-mongoose');
+const passportLocalMongoose = require('password-local-mongoose'); // adds additional fields to schema and any additional mehods for using passwords
 
 const userSchema = new Schema({
-
+    email: {
+        type: String, 
+        unique: true, 
+        lowercase: true, 
+        trim: true, 
+        validate: [validator.isEmail, 'Invalid Email Address'], 
+        required: 'Please Supply an Email Address'
+    },
+    name: {
+        type: String, 
+        required: 'Please Supply a Name',
+        trim: true
+    }
 });
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler); // gives the user nicer error messages
 
 module.exports = mongoose.model('User', userSchema);
