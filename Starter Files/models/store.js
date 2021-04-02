69c128fc3e76c167fc4373ff28d49c53a098bb28
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-01 22:59:37 -0500

    feat(store.js): added a slug of a number when repeated store name have been used

--- Starter Files/models/store.js
+++ Starter Files/models/store.js
@@ -34,15 +34,31 @@ const storeSchema = new mongoose.Schema({
     }, 
     photo: String
 });
-storeSchema.pre('save', function(next) { // save will not happen until work is done within this function
+storeSchema.pre('save', async function(next) { // save will not happen until work is done within this function
     if (!this.isModified('name')) {
         next(); // skip it 
         return; // stop this function from running
     }
     this.slug = slug(this.name); 
+    // find other stores that have a slug of wes, wes-1, wes-2
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+
     next();
     // TODO make more resiliant so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function() {
+    return this.aggregate([
+        { $unwind: '$tags' },
+        { $group: { _id: '$tags', count: { $sum: 1 } } },
+        { $sort: { count: -1 }}
+    ]);
+}
+
 
 module.exports = mongoose.model('Store', storeSchema);                                            // imports object with many properties instead of a functon 
 
