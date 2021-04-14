51a0a1f816f9c46e9b299b263324779727763512
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-14 15:55:18 -0500

    feat(store.js): add a store schema index

--- Starter Files/models/store.js
+++ Starter Files/models/store.js
@@ -40,30 +40,61 @@ const storeSchema = new mongoose.Schema({
         // Give a relationship between the user and the store in MongoDB
     }
 });
-storeSchema.pre('save', async function(next) { // save will not happen until work is done within this function
+
+// Define our indexes, allow user to search by keyword in the name and despription
+storeSchema.index({
+    name: 'text',
+    description: 'text'
+  });
+  
+  storeSchema.index({ location: '2dsphere' });
+  
+  storeSchema.pre('save', async function(next) {
     if (!this.isModified('name')) {
-        next(); // skip it 
-        return; // stop this function from running
+      next(); // skip it
+      return; // stop this function from running
     }
-    this.slug = slug(this.name); 
+    this.slug = slug(this.name);
     // find other stores that have a slug of wes, wes-1, wes-2
     const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
     const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
-    if(storesWithSlug.length) {
-        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    if (storesWithSlug.length) {
+      this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
     }
-
     next();
     // TODO make more resiliant so slugs are unique
-});
-
-storeSchema.statics.getTagsList = function() {
+  });
+  
+  storeSchema.statics.getTagsList = function() {
     return this.aggregate([
-        { $unwind: '$tags' },
-        { $group: { _id: '$tags', count: { $sum: 1 } } },
-        { $sort: { count: -1 }}
+      { $unwind: '$tags' },
+      { $group: { _id: '$tags', count: { $sum: 1 } } },
+      { $sort: { count: -1 } }
     ]);
+  }
+
+storeSchema.statics.getTopStores = function() {
+    return this.aggregate([
+        // aggregate is a query function, it is not mongoose specific so we can't use the virtual reviews. Goes right to MongoDB
+        { $lookup: 
+            {from: 'reviews', localField: '_id', 
+            foreignField: 'store', as: 'reviews'}},
+        // Lookup Stores and populate their reviews
+        { $match: { 'reviews.1': { $exists: true }}} // reviews.1 access the second item in the reviews
+        // Filter for only items that have 2 or more reviews
+        // Add the average reviews field
+        // Sort it by our new field, highest reviews first
+        // Limit to at most 10
+
+    ])
 }
+
+// find review where the stores _id property === reviews store property
+storeSchema.virtual('reviews', {
+    ref: 'Review', // what model to link?
+    localField: '_id', // which field on the store?
+    foreignField: 'store', // which field on the review?
+});
 
 
 module.exports = mongoose.model('Store', storeSchema);                                            // imports object with many properties instead of a functon 
