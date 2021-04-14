663fbb7386d4e80c4275f8e57a797c263823be50
Felicia Brenner <felicia.brenner514@gmail.com>
2021-04-14 15:55:58 -0500

    feat(User.js) added a Hearts object id for the user

--- Starter Files/models/User.js
+++ Starter Files/models/User.js
@@ -4,32 +4,36 @@ mongoose.Promise = global.Promise;
 const md5 = require('md5');
 const validator = require('validator');
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('passport-local-mongoose'); // adds additional fields to schema and any additional mehods for using passwords
+const passportLocalMongoose = require('passport-local-mongoose'); // adds additional fields to schema and any additional methods for using passwords
 
 const userSchema = new Schema({
-    email: {
-        type: String, 
-        unique: true, 
-        lowercase: true, 
-        trim: true, 
-        validate: [validator.isEmail, 'Invalid Email Address'], 
-        required: 'Please Supply an Email Address'
-    },
-    name: {
-        type: String, 
-        required: 'Please Supply a Name',
-        trim: true
-    }, 
-    resetPasswordToken: String, 
-    resetPasswordExpires: Date,
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please Supply an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please supply a name',
+    trim: true
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  hearts: [
+    { type: mongoose.Schema.ObjectId, ref: 'Store' }
+  ]
 });
 
 userSchema.virtual('gravatar').get(function() {
-    const hash = md5(this.email);
-    return `https://gravatar.com/avatar/${hash}?s=200`;
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`;
 });
 
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
-userSchema.plugin(mongodbErrorHandler); // gives the user nicer error messages
+userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
+
